e6aedfbe49b907e0e238545ee3f3bb5912df50ad
Sean O'Neil <sean.oneil78@hotmail.com>
2020-03-01 00:11:18 -0800

    ...

--- UWP-Transitions/MainPage.xaml.cs
+++ UWP-Transitions/MainPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +29,34 @@ namespace UWP_Transitions
         public MainPage()
         {
             this.InitializeComponent();
+        }
+        PixelShaderEffect DissolveEffect;
+        PixelShaderEffect RipplesPixelShaderEffect;
+
+        private void canvas_CreateResources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
+        {
+            args.TrackAsyncAction(canvas_CreateResourcesAsync(sender).AsAsyncAction());
+        }
+
+        async Task canvas_CreateResourcesAsync(CanvasControl args)
+        {
+            DissolveEffect = new PixelShaderEffect(await ReadAllBytes("Assets/Shaders/Dissolve.bin"));
+            DissolveEffect.Properties["feather"] = 0.1f;
+            RipplesPixelShaderEffect = new PixelShaderEffect(await ReadAllBytes("Assets/Shaders/Ripples.bin"));
+        }
+
+        private void canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
+        {
+
+        }
+
+        static async Task<byte[]> ReadAllBytes(string filename)
+        {
+            var uri = new Uri("ms-appx:///" + filename);
+            var file = await StorageFile.GetFileFromApplicationUriAsync(uri);
+            Windows.Storage.Streams.IBuffer buffer = await FileIO.ReadBufferAsync(file);
+
+            return buffer.ToArray();
         }
     }
 }
